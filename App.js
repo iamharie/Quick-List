fece9fa00aeea44fa13a5689d0924afdef9bfde7
Mohan <telecomst7@wichita.edu>
2024-07-18 15:23:50 -0500

    close button added; implemented JS logic

--- App.js
+++ App.js
@@ -14,19 +14,26 @@ btn.addEventListener("click", function (e) {
     <div class="task-added">
         <input type="checkbox" class="check" />
         <h6 class="text-strike">${input.value}</h6>
+        <button class="closeBtn">❌</button>
     </div>
   `;
 
   //logic
 
   if (input.value !== "") {
-    console.log("not empty");
+    // console.log("not empty");
     htmlSpace.insertAdjacentHTML("afterbegin", html);
     input.value = "";
     const strikeMe = document.querySelector(".text-strike");
     const checkbox = document.querySelector(".check");
+    const closeBtn = document.querySelector(".closeBtn");
+    const addedTask = document.querySelector(".task-added");
     checkbox.addEventListener("change", function () {
       strikeMe.classList.toggle("strike");
+    });
+    closeBtn.addEventListener("click", function () {
+      addedTask.remove();
+      // console.log(addedTask);
     });
   } else {
     console.log("empty");
